a846924eac2cc5172352d46ae5dd7d6b14808065
ompangale2007-oap <ompangale2007@gmail.com>
2026-06-06 22:11:51 +0530

    Add files via upload

---
+++ Basics/combination.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int function(int x){
+int product=1;
+for(int i=1;i<=x;i++){
+product *= i;
+
+}
+return product;
+}
+int main()
+{
+    int n;
+    cout<<"ENTER n : ";
+    cin>>n;
+    int r;
+    cout<<"ENTER r : ";
+    cin>>r;
+    
+    cout<< function(n)/ (function(r) * function(n-r));
+}
---
+++ Basics/swapingbypointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+void swap(int* x, int* y) {
+    int temp = *x; 
+    *x = *y;    
+    *y = temp;    
+}
+
+int main() {
+    int a = 60;
+    int b = 20;
+
+    cout<< a << b << endl;
+
+    
+    swap(&a, &b);
+
+    cout << a <<  b << endl;
+
+    
+}
