96a94f17e1b9fe736fe89b98a4914f3e0e0ed3af
ompangale2007-oap <ompangale2007@gmail.com>
2026-06-26 19:16:00 +0530

    Add files via upload

---
+++ Basics/vectors.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int> v(6);
+for(int i=0;i<6;i++){
+    cin>>v[i];
+}
+int x;
+cout<<"enter x :  ";
+cin>>x;
+int occ = -1;
+for(int i=0;i<v.size();i++)
+if(v[i]==x){
+    occ=i;
+}
+cout<<occ<<endl;
+}
