0dd619259d9bd5adccc93e7273ff851829b30825
ompangale2007-oap <ompangale2007@gmail.com>
2026-06-26 19:16:24 +0530

    Add files via upload

---
+++ OOPs/strictly.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int> v(6);
+for(int i=0;i<6;i++){
+    cin>>v[i];
+}
+int x;
+cout<<"enter x :  ";
+cin>>x;
+int count=0;
+for(int i=0;i<v.size();i++){
+if(v[i]>x){
+    count++;
+}
+}
+cout<<count;
+}
